6465719f744241e049dd0b9bd5248553390ea164
Sohaib El Bouhtani <sohaib.elbouhtani@student.vinci.be>
2024-09-24 15:11:27 +0200

    resolve: solution 1.1-1.2

--- exercices/1.1-1.2/part1/src/App.jsx
+++ exercices/1.1-1.2/part1/src/App.jsx
@@ -1,6 +1,28 @@
-import Header from './components/Header'
-import Content from './components/Content'
-import Total from './components/Total'
+const Header = (props) =>{
+  return (<h1>{props.course}</h1>)
+}
+
+const Part = (props) =>{
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Content = (props) =>{
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1}/>
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
 
 
 const App = () => {
@@ -15,8 +37,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content />
-      <Total total={exercises1+exercises2+exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
 }
--- exercices/1.1-1.2/part1/src/components/Total/Total.jsx
+++
@@ -1,6 +0,0 @@
-
-const Total = (props) => {
-    return (
-      <p>Number of exercises {props.total}</p>
-    )
-  }
