fcbb9162f022a5a3c71c0bf760b7ceafb99dd833
Sohaib El Bouhtani <sohaib.elbouhtani@student.vinci.be>
2024-09-24 21:53:01 +0200

    refactor: component

--- exercices/1.1-1.2/part1/src/Components/App/App.jsx
+++ exercices/1.1-1.2/part1/src/Components/App/App.jsx
@@ -1,30 +1,3 @@
-const Header = (props) =>{
-  return (<h1>{props.course}</h1>)
-}
-
-const Part = (props) =>{
-  return (
-    <p>{props.part} {props.exercises}</p>
-  )
-}
-
-const Content = (props) =>{
-  return (
-    <div>
-      <Part part={props.part1} exercises={props.exercises1}/>
-      <Part part={props.part2} exercises={props.exercises2}/>
-      <Part part={props.part3} exercises={props.exercises3}/>
-    </div>
-  )
-}
-
-const Total = (props) =>{
-  return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-  )
-}
-
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
