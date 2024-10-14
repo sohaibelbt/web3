b774be444f1198ba6a139c914cb295ea4b622a62
sohaib <sohaib.elbouhtani@student.vinci.be>
2024-10-14 21:21:36 +0200

    refactoring: move module2 on tutoriels

---
+++ tutoriels/module2/src/components/Main/DrinkCard.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+const DrinkCard = ({image,title,children}) => {
+  return (
+    <div className="drink-card">
+      <img
+        src={image}
+        alt={title}
+        className="drink-image"
+        width="50"
+      />
+      <h2>{title}</h2>
+      <div className="drink-details">{children}</div>
+    </div>
+  );
+};
+
+export default DrinkCard;
---
+++ tutoriels/module2/src/components/Main/DrinkMenu.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { ReactElement } from "react";
+
+const DrinkMenu = ({title,children}) => {
+  return (
+    <div className="drink-menu">
+      <h4>{title}</h4>
+      <div className="drink-items">{children}</div>
+    </div>
+  );
+};
+
+export default DrinkMenu;
