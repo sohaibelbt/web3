736d536a65156281c9f143d8ebd3d549ed742f47
sohaib <sohaib.elbouhtani@student.vinci.be>
2024-11-04 23:02:31 +0100

    1.1-1.2 : import image

--- exercices/module1/vite-project/src/components/Header/Header.jsx
+++ exercices/module1/vite-project/src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
+import image from './LOGO HE VINCI.png';
+
 const Header = () => {
-    return (
-      <h1>Half Stack application development</h1>
-    )
+    return <>
+      <div>
+        <img src={image}/>
+        <h1>Half Stack application development</h1>
+      </div>
+    </>
 }
 
 export default Header;
