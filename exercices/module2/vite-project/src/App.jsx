966ef2d4b39ef027cb3000dbbfd1c9a8d856bb49
sohaib <sohaib.elbouhtani@student.vinci.be>
2024-11-05 00:04:26 +0100

    new: ex2.1-destructuring

--- exercices/module2/vite-project/src/App.jsx
+++ exercices/module2/vite-project/src/App.jsx
@@ -3,8 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Title = (props) => {
+  return (
+    <h1>{props.title1} + {props.title2}</h1>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  let mess;
+  if (count >= 10) {
+    mess = <p>You are a master in the art of clicking!</p>;
+  }
 
   return (
     <>
@@ -16,10 +26,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Title title1={'Vite'} title2={'React'}/>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count +1)}>
           count is {count}
+          {mess}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
