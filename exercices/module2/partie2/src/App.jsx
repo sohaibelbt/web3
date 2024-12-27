59c6ceb8d80de410ccf340c46e6d7402c9bb75ff
sohaib <sohaib.elbouhtani@student.vinci.be>
2024-12-27 01:42:26 +0100

    new:ex2.1-destructuring

--- exercices/module2/partie2/src/App.jsx
+++ exercices/module2/partie2/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <ClickCounter />
+        <ClickCounter title={"You are a master in the art of clicking !"}/>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- exercices/module2/partie2/src/components/ClickCounter/ClickCounter.jsx
+++ exercices/module2/partie2/src/components/ClickCounter/ClickCounter.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-const ClickCounter = () => {
+import { useState, useEffect } from 'react'
+const ClickCounter = ({title}) => {
     const [count, setCount] = useState(0);
+    const [countAt10, setCountAt10] = useState(false);
+
+    useEffect(() => {
+        if (count === 10) {
+            setCountAt10(true);
+        }
+    }, [count]); // Exécute cet effet seulement lorsque count change
 
     return (
-        <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-        </button>
+        <>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p>
+                {countAt10 ? `${title}` : ``}
+            </p>
+        </>        
     )
 }
 
