3f02a3962e9f99891268f73218659200e06937f8
sohaib <sohaib.elbouhtani@student.vinci.be>
2024-12-29 21:03:14 +0100

    new: ex2.2

--- exercices/module2/exercice2.2/src/components/App/App.jsx
+++ exercices/module2/exercice2.2/src/components/App/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
+import Box from 'components/Box/Box'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,12 +17,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Box />
+        <Box />
+        <Box />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
